b8f82d2a571575a066addba334caaa359cccbdf9
Barbara Gaweł-Kucab <bgawelkucab@gmail.com>
2023-04-01 15:56:30 +0200

    feat: admin panel && local storage setup (#3)

--- frontend/src/components/Admin.tsx
+++ frontend/src/components/Admin.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import { UserWithName } from "../types";
+import styled from "styled-components";
+
+const USER_NUMBERS = 5;
+
 export const Admin = () => {
-  return <div>Admin panel here </div>;
+  const [newUsers, setNewUsers] = useState<UserWithName[]>([]);
+
+  const postNewUsers = () => {
+    console.log(newUsers);
+  };
+
+  const saveUser = (idx: number, e: any) => {
+    const newArr = [...newUsers];
+    newArr[idx] = e.target.value;
+    setNewUsers(newArr);
+  };
+
+  return (
+    <div>
+      <p>Admin panel</p>
+      <div>
+        {[...Array(USER_NUMBERS)].map((i) => (
+          <input
+            name="name"
+            key={i}
+            onChange={(e) => saveUser(i, e)}
+            placeholder="user name"
+          />
+        ))}
+      </div>
+      <button onClick={postNewUsers}>Create office</button>
+    </div>
+  );
 };
